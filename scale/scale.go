f48ce362706b8c47fdc668684a5eb1364d128114
Engin Vardar <engin.vardar@global-fashion-group.com>
2021-04-26 10:08:28 +0200

    add prefix to all logs

--- scale/scale.go
+++ scale/scale.go
@@ -73,7 +73,7 @@ func (p *PodAutoScaler) Scale(ctx context.Context, numMessages int) *ScalingResu
 	desiredReplicas := p.getDesiredReplicaCount(numMessages)
 
 	if *currentReplicas == desiredReplicas {
-		log.Infof("Same as desired replicas. Current replicas: %d Desired replicas: %d", *deployment.Spec.Replicas, desiredReplicas)
+		log.Infof("[autoscaler] Same as desired replicas. Current replicas: %d Desired replicas: %d", *deployment.Spec.Replicas, desiredReplicas)
 		return &ScalingResult{
 			Err:            errors.Wrap(err, "Failed to get deployment from kube server, no scale down occured"),
 			ScalingSkipped: true,
@@ -83,7 +83,7 @@ func (p *PodAutoScaler) Scale(ctx context.Context, numMessages int) *ScalingResu
 	deployment.Spec.Replicas = &desiredReplicas
 
 	if p.DryRun {
-		log.Infof("[DryRun] would scale deployment %s to %d replicas", p.Deployment, desiredReplicas)
+		log.Infof("[autoscaler] [DryRun] would scale deployment %s to %d replicas", p.Deployment, desiredReplicas)
 		return &ScalingResult{
 			Err:            errors.Wrap(err, "Failed to get deployment from kube server, no scale down occured"),
 			ScalingSkipped: false,
@@ -98,7 +98,7 @@ func (p *PodAutoScaler) Scale(ctx context.Context, numMessages int) *ScalingResu
 		}
 	}
 
-	log.Infof("Scaling successful. Replicas: %d", *deployment.Spec.Replicas)
+	log.Infof("[autoscaler] Scaling successful. Replicas: %d", *deployment.Spec.Replicas)
 	return &ScalingResult{
 		Err:            nil,
 		ScalingSkipped: false,
@@ -109,12 +109,12 @@ func (p *PodAutoScaler) getDesiredReplicaCount(numMessages int) int32 {
 	desiredReplicas := int(math.Ceil(float64(numMessages) / float64(p.MessagePerPod)))
 
 	if p.ZeroScaling == false && desiredReplicas < int(p.Min) {
-		log.Infof("desired replicas are less than min pods resetting to min. Min pod: %d Desired replicas: %d", p.Min, desiredReplicas)
+		log.Infof("[autoscaler] desired replicas are less than min pods resetting to min. Min pod: %d Desired replicas: %d", p.Min, desiredReplicas)
 		desiredReplicas = int(p.Min)
 	}
 
 	if desiredReplicas > int(p.Max) {
-		log.Infof("desired replicas are more than max pods resetting to max. Max pod: %d Desired replicas: %d", p.Max, desiredReplicas)
+		log.Infof("[autoscaler] desired replicas are more than max pods resetting to max. Max pod: %d Desired replicas: %d", p.Max, desiredReplicas)
 		desiredReplicas = int(p.Max)
 	}
 
